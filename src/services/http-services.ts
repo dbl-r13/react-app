b709530e6fa7cd9edc2a0a33e2cc066b12461c6f
Ryan Richard <tank1332@me.com>
2024-02-17 23:42:56 -0700

    feat: created file and added logic to make using HTTP Requests easier to find, change and create.

---
+++ src/services/http-services.ts
@@ -0,0 +1,47 @@
+import apiClient, { CanceledError,Axios, AxiosError, } from "./api-client";
+
+ interface Entity {
+    id: number;
+
+}
+
+
+class HttpService{
+    endpoint: string;
+    constructor(endpoint: string){
+        this.endpoint = endpoint;
+    }
+    
+    getAll<T>() {
+
+    const controller = new AbortController();
+
+   const request = apiClient.get<T[]>("this.endpoint", { signal: controller.signal,});
+
+      return { request, cancel: () => controller.abort()
+      }
+    }
+
+    add<T>(entity: T){
+       return apiClient
+      .post(this.endpoint, entity)
+    }
+
+    delete(id: number){
+      return apiClient
+      .delete(`${this.endpoint}/${id}`)
+    }
+
+    update<T extends Entity>(id:number,updatedUser: {}){
+        return apiClient
+        .patch(
+          `/users/${id}`,
+          updatedUser
+        );
+    }
+
+}
+
+const create = (endpoint: string) =>  new HttpService(endpoint);
+
+export default create;
