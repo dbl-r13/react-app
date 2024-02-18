c0d3a0940d206500c87980de627e0ef04be2712e
Ryan Richard <tank1332@me.com>
2024-02-17 23:42:07 -0700

    refactor: moved all logic for enpoints to another file and made this one simpler to use.

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -1,4 +1,6 @@
-import apiClient, { CanceledError,Axios, AxiosError, } from "./api-client";
+import apiClient from "./api-client";
+import create from "./http-services"
+
 
 //TODO: add AxiosUser component and make sure all things function properly once added.
   //AxiosUser Section: added on page without component created
@@ -7,47 +9,5 @@ import apiClient, { CanceledError,Axios, AxiosError, } from "./api-client";
     name: string;
   }
 
-class UserService{
-    getAllAxiosUsers() {
-        /** const fetchUsers = async () => {
-     try {
-       const res = await axios.get<AxiosUser[]>(
-         "https://jsonplaceholder.typicode.com/users",
-         { signal: controller.signal }
-         );
-         setUsers(res.data);
-        } catch (err) {
-          if (err instanceof CanceledError) return;
-          setAxiosError((err as AxiosError).message);
-        }
-      };
-      fetchUsers();
-      */
 
-    const controller = new AbortController();
-
-   const request = apiClient.get<AxiosUser[]>("/users", { signal: controller.signal,});
-   request.then(data => console.log(data))
-      return { request, cancel: () => controller.abort()
-      }
-    }
-
-    addAxiosUser(newUser:AxiosUser){
-       return apiClient
-      .post(`/users`, newUser)
-    }
-    deleteAxiosUser(id: number){
-      return apiClient
-      .delete(`/users/${id}`)
-    }
-    updateAxiosUser(id:number,updatedUser: {}){
-        return apiClient
-        .patch(
-          `/users/${id}`,
-          updatedUser
-        );
-    }
-
-}
-
-export default new UserService();
+export default create('/users/')
